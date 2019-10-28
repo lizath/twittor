4f7684d84db527562b614d7ca01c4bd6d18fd605
Liza Ramos <liza.ramos@appbogados.com>
2019-10-28 17:22:49 -0600

    correcion Notch

--- sw.js
+++ sw.js
@@ -1,8 +1,8 @@
 //imports
 importScripts('js/sw-utils.js');
 
-const STATIC_CACHE = 'static_v3';
-const DYNAMIC_CACHE = 'dynamic_v1';
+const STATIC_CACHE = 'static_v4';
+const DYNAMIC_CACHE = 'dynamic_v2';
 const INMUTABLE_CACHE = 'inmutable_v1';
 
 const APP_SHELL = [
@@ -45,6 +45,9 @@ self.addEventListener('activate', e=>{
 
             // static-v4
             if (  key !== STATIC_CACHE && key.includes('static') ) {
+                return caches.delete(key);
+            }
+			if (  key !== DYNAMIC_CACHE && key.includes('dynamic') ) {
                 return caches.delete(key);
             }
 
